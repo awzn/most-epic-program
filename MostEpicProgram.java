ba478cf08db90aaceb88cae704d9abee7860cd95
qiandasen <48695712+qiandasen@users.noreply.github.com>
2019-03-18 13:37:49 -0700

    Add files via upload

---
+++ MostEpicProgram.java
@@ -0,0 +1,20 @@
+/**
+   WARNING
+   extremely epic
+
+   only people who are very cool can run
+
+   @author qiandasen
+*/
+
+public class MostEpicProgram {
+   public static void main(String[] args){
+      System.out.println(" /\\");
+      System.out.println("/  \\");
+      System.out.println("||_|");
+      System.out.println("\\_ \\");
+      System.out.println("|| |");
+      System.out.println("\\  /");
+      System.out.println(" \\/");           
+   }
+}
